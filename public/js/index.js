6e87415c47935c917451ffacf0930225928eef4a
leonlxli <Li.Lxiao@yahoo.com>
2016-04-21 15:46:53 -0700

    part 1 done

--- public/js/index.js
+++ public/js/index.js
@@ -1,79 +1,188 @@
-(function($) {
-  "use strict";
+(function(d3) {
+    "use strict";
 
-  var data = [
-    { name: "Lolita's", rating: 7.5 },
-    { name: "Lucha Libre", rating: 8 },
-    { name: "Puesto", rating: 9.5 },
-    { name: "Rubio's", rating: 4 },
-    { name: "Taco Bell", rating: 3 },
-    { name: "Taco Stand", rating: 8.5 },
-    { name: "Taco's, El Gordo", rating: 9 },
-    { name: "Oscar's", rating: 9 },
-    { name: "Rigoberto's", rating: 6 },
-    { name: "Galaxy Taco", rating: 6.5 },
-  ];
+    var data = [{
+        name: "Lolita's",
+        rating: 7.5
+    }, {
+        name: "Lucha Libre",
+        rating: 8
+    }, {
+        name: "Puesto",
+        rating: 9.5
+    }, {
+        name: "Rubio's",
+        rating: 4
+    }, {
+        name: "Taco Bell",
+        rating: 3
+    }, {
+        name: "Taco Stand",
+        rating: 8.5
+    }, {
+        name: "Taco's, El Gordo",
+        rating: 9
+    }, {
+        name: "Oscar's",
+        rating: 9
+    }, {
+        name: "Rigoberto's",
+        rating: 6
+    }, {
+        name: "Galaxy Taco",
+        rating: 6.5
+    }, ];
 
-  // Defining the margins and chart size
-  // See margin conventions for more information
-  var margin = {top: 20, right: 10, bottom: 100, left: 40},
-      width = 960 - margin.right - margin.left,
-      height = 500 - margin.top - margin.bottom;
+    // Defining the margins and chart size
+    // See margin conventions for more information
+    var margin = {
+            top: 20,
+            right: 10,
+            bottom: 100,
+            left: 40
+        },
+        width = 960 - margin.right - margin.left,
+        height = 500 - margin.top - margin.bottom;
 
-  var innerWidth  = width  - margin.left - margin.right;
-  var innerHeight = height - margin.top  - margin.bottom;
+    var innerWidth = width - margin.left - margin.right;
+    var innerHeight = height - margin.top - margin.bottom;
 
-  // TODO: Input the proper values for the scales
-  var xScale = d3.scale.ordinal().rangeRoundBands([0, 10], 0);
-  var yScale = d3.scale.linear().range([30, 0]);
+    // TODO: Input the proper values for the scales
+    var xScale = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+    var yScale = d3.scale.linear().range([height, 0]);
 
-  // Define the chart
-  var chart = d3
-                .select(".chart")
-                .append("svg")
-                .attr("width", width + margin.right + margin.left)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" +  margin.left + "," + margin.right + ")");
+    // Define the chart
+    var chart = d3
+        .select(".chart")
+        .append("svg")
+        .attr("width", width + margin.right + margin.left)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.right + ")");
 
-  // Render the chart
-  xScale.domain(data.map(function (d){ return d.name; }));
+    // Render the chart
+    xScale.domain(data.map(function(d) {
+        return d.name;
+    }));
 
-  // TODO: Fix the yScale domain to scale with any ratings range
-  yScale.domain([0, 5]);
+    // TODO: Fix the yScale domain to scale with any ratings range
+    yScale.domain([0, d3.max(data, function(d) {
+        return d.rating;
+    })]);
 
-  // Note all these values are hard coded numbers
-  // TODO:
-  // 1. Consume the taco data
-  // 2. Update the x, y, width, and height attributes to appropriate reflect this
-  chart
-    .selectAll(".bar")
-    .data([10, 20, 30, 40])
-    .enter().append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d, i) { return i*100; })
-    .attr("width", 100)
-    .attr("y", function(d) { return 0; })
-    .attr("height", function(d) { return d*10; });
+    // Note all these values are hard coded numbers
+    // TODO:
+    // 1. Consume the taco data
+    // 2. Update the x, y, width, and height attributes to appropriate reflect this
+    chart
+        .selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d, i) {
+            return xScale(d.name);
+        })
+        .attr("width", xScale.rangeBand())
+        .attr("y", function(d) {
+            return yScale(d.rating);
+        })
+        .attr("height", function(d) {
+            return height - yScale(d.rating);
+        });
 
-  // Orient the x and y axis
-  var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-  var yAxis = d3.svg.axis().scale(yScale).orient("left");
+    // Orient the x and y axis
+    var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+    var yAxis = d3.svg.axis().scale(yScale).orient("left");
 
-  // TODO: Append X axis
-  chart
-    .append("g");
-
-
-  // TODO: Append Y axis
-  chart
-    .append("g");
+    // TODO: Append X axis
+    chart
+        .append("g").attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
 
-  // ASSIGNMENT PART 1B
-  // Grab the delphi data from the server
-  $.get( "/delphidata", function(data) {
-    console.log("Data", data);
-  });
+    // TODO: Append Y axis
+    chart.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Frequency");
 
-})($);
+
+    // ASSIGNMENT PART 1B
+    // Grab the delphi data from the server
+    d3.json("/delphidata", function(err, dat) {
+        console.log("data");
+        if (err) {
+            console.log(err);
+            return;
+        }
+        data = dat;
+        var chart = d3
+            .select(".chart")
+            .append("svg")
+            .attr("width", width + margin.right + margin.left)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.right + ")");
+
+        // Render the chart
+        xScale.domain(data.map(function(d) {
+            return d.gender;
+        }));
+
+        // TODO: Fix the yScale domain to scale with any ratings range
+        yScale.domain([0, d3.max(data, function(d) {
+            return d.number_of_respondents;
+        })]);
+
+        // Note all these values are hard coded numbers
+        // TODO:
+        // 1. Consume the taco data
+        // 2. Update the x, y, width, and height attributes to appropriate reflect this
+        chart
+            .selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d, i) {
+                return xScale(d.gender);
+            })
+            .attr("width", xScale.rangeBand())
+            .attr("y", function(d) {
+                return yScale(d.number_of_respondents);
+            })
+            .attr("height", function(d) {
+                return height - yScale(d.number_of_respondents);
+            });
+
+        // Orient the x and y axis
+        var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+        var yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+        // TODO: Append X axis
+        chart
+            .append("g").attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+
+        // TODO: Append Y axis
+        chart.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Frequency");
+    });
+
+    console.log("done");
+
+})(d3);
--- server.js
+++ server.js
@@ -14,36 +14,47 @@ dotenv.load();
 
 //connect to database
 var conString = process.env.DATABASE_CONNECTION_URL;
+var client = new pg.Client(conString);
+client.connect(function(err) {
+    if (err) {
+        console.error('could not connect to postgres', err);
+    } else {
+        console.log("Successfully connected to postgres")
+    }
+});
 
 //Configures the Template engine
-app.engine('html', handlebars({ defaultLayout: 'layout', extname: '.html' }));
+app.engine('html', handlebars({
+    defaultLayout: 'layout',
+    extname: '.html'
+}));
 app.set("view engine", "html");
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
-app.use(session({ secret: 'keyboard cat',
-                  saveUninitialized: true,
-                  resave: true}));
+app.use(session({
+    secret: 'keyboard cat',
+    saveUninitialized: true,
+    resave: true
+}));
 
 //set environment ports and start application
 app.set('port', process.env.PORT || 3000);
 
 //routes
-app.get('/', function(req, res){
-  res.render('index');
+app.get('/', function(req, res) {
+    res.render('index');
 });
 
-app.get('/delphidata', function (req, res) {
-  // TODO
-  // Connect to the DELPHI Database and return the proper information
-  // that will be displayed on the D3 visualization
-  // Table: Smoking Prevalance in Adults
-  // Task: In the year 2003, retrieve the total number of respondents
-  // for each gender. 
-  // Display that data using D3 with gender on the x-axis and 
-  // total respondents on the y-axis.
-  return { delphidata: "No data present." }
+app.get('/delphidata', function(req, res) {
+  console.log("calling");
+    client.query("SELECT gender, number_of_respondents FROM cogs121_16_raw.cdph_smoking_prevalence_in_adults_1984_2013 WHERE year = 2003 ",function(err,dat){
+        res.json(dat.rows);
+    });
+
 });
 
 
